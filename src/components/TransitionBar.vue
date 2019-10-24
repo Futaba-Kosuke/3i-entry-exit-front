998c4deb593d7de09d3dbe6fc2bac0893ea469ef
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-24 17:25:28 +0900

    [add] TransitionBarを生成して内容を書き込みました。 #17
    特に無いです。

---
+++ src/components/TransitionBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row
+    justify="center"
+    no-gutters
+  >
+    <v-btn to="/qr_reader" text color="white" class="my-2" exact>ゲーム</v-btn>
+    <v-btn to="/ranking" text color="white" class="my-2" exact>ランキング</v-btn>
+  </v-row>
+</template>
