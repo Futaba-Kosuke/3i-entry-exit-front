a3b7bb8715810c7654c3701d463aec6124ce4929
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-26 00:59:33 +0900

    [add] ニックネームが既存のとき、QR読み込みカメラを起動できないようにしました。 #3
    既存のニックネームを防ぎます。

--- src/views/QrReader.vue
+++ src/views/QrReader.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container>
-    <QrReaderCamera/>
-  </v-container>  
+    <v-text-field
+      v-model="user_name"
+      label="ニックネームを入力してね"
+      :hint="hintMessage"
+      persistent-hint
+    />
+    <v-col v-if="isSearch()">
+      <QrReaderCamera :user_name="user_name"/>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
@@ -10,6 +18,25 @@ import QrReaderCamera from '../components/QrReaderCamera'
 export default {
   components: {
     QrReaderCamera,
-  }  
+  },
+  data () {
+    return {
+      user_name: '',
+      name_list: this.$store.state.name_list,
+    }
+  },
+  computed: {
+    hintMessage() {
+      if (this.isSearch()) return 'このニックネームは有効です！'
+      else return 'このニックネームは無効です！'
+    }
+  },
+  methods: {
+    isSearch() {
+      if (this.user_name === '') return false
+      const result = this.name_list.indexOf(this.user_name)
+      return result === -1
+    },
+  }
 }
 </script>
