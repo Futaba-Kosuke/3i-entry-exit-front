be2b2d82e4d85734c48122f14f87d6dab2070a37
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-29 17:49:52 +0900

    [add] axiosでランキングデータを取得するようにしました #20

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -6,11 +6,16 @@
 
 <script>
 import RankingTable from '../components/RankingTable'
+import axios from 'axios'
 
 export default {
   name: 'RankingView',
   components: {
     RankingTable,
+  },
+  created: async function() {
+    const ranking_data = await axios.get('https://server-3i-entry-exit.herokuapp.com/api/v1/ranking')
+    this.$store.commit('updateRankingData', ranking_data.data)
   }
 }
 </script>
