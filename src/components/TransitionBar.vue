c1f7683bf9eb16141ac73860a064b9d3aa0f09da
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-30 08:38:33 +0900

    [fix] ホーム画面にQRを設定しました #27

--- src/components/TransitionBar.vue
+++ src/components/TransitionBar.vue
@@ -3,7 +3,7 @@
     justify="space-around"
     no-gutters
   >
-    <v-btn to="/qr_reader" text color="white" class="my-2" exact>QR読み込み</v-btn>
+    <v-btn to="/" text color="white" class="my-2" exact>QR読み込み</v-btn>
     <v-btn to="/ranking" text color="white" class="my-2" exact>ランキング</v-btn>
   </v-row>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/qr_reader',
+    path: '/',
     name: 'qr_reader',
     component: () => import('../views/QrReader.vue')
   },
